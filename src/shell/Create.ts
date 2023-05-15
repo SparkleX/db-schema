14c5ebd1ad3437515b8cecf37dd0095ad973c63c
Sun Xufei <xufei.sun@sap.com>
2023-05-15 16:58:27 +0800

    dev

--- src/shell/Create.ts
+++ src/shell/Create.ts
@@ -1,2 +1,13 @@
 
-console.debug("create");
+import { Sqlite3DdlBuilder } from "../ddl/Sqlite3DdlBuilder.js";
+import jsonfile from "jsonfile";
+import { default as glob } from "glob";
+
+const builder = new  Sqlite3DdlBuilder();
+
+const files = glob.sync(`./*.table.json`);
+for (const file of files) {
+	const oTable = jsonfile.readFileSync(file);
+	const sql = builder.createTable(oTable) + ";";
+	console.debug(sql);
+}
